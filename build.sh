82c95f132f2a32335b58d192d2c8eda663dd2e69
davidweilers <david@weilers.nl>
2026-06-21 18:31:45 +0200

    ...

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cargo build --target aarch64-unknown-linux-gnu -r
+
+scp target/aarch64-unknown-linux-gnu/release/onthoudhet-kiosk david@raspberrypi.fritz.box:
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use iced::time::milliseconds;
 use ::image as img;
 use ::image::ColorType;
 use iced::widget::{button, center_y, column, container, image, row, text, text_input};
@@ -16,6 +17,13 @@ enum Message {
     Screenshotted(Screenshot),
     Png,
     PngSaved(Result<String, PngError>),
+    Scp,
+    Tick,
+}
+
+enum State {
+    Idle,
+    Ticking { last_tick: std::time::Instant },
 }
 
 struct App {
@@ -24,6 +32,10 @@ struct App {
     screenshot: Option<(Screenshot, image::Handle)>,
     saved_png_path: Option<Result<String, PngError>>,
     png_saving: bool,
+    scp_sending: bool,
+    state: State,
+    tick_duration: std::time::Duration,
+    tick_count: u32,
 }
 
 impl App {
@@ -38,15 +50,33 @@ impl App {
                 screenshot: None,
                 saved_png_path: None,
                 png_saving: false,
+                scp_sending: false,
+                state: State::Idle,
+                tick_duration: std::time::Duration::from_millis(100),
+                tick_count: 0,
             },
             Task::done(Message::WebView(Action::CreateView(PageType::Url(
-                "https://onthoudhetv2.weilers.nl/".to_string(),
+                "https://onthoudhetv3.weilers.nl/".to_string(),
             )))),
         )
     }
 
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
+            Message::Tick => {
+                self.tick_count += 1;
+                if let State::Ticking { last_tick } = self.state {
+                    let now = std::time::Instant::now();
+                    self.tick_duration = now.duration_since(last_tick);
+                    self.state = State::Ticking { last_tick: now };
+                    if self.tick_count % 80 == 0 {
+                        println!("Tick duration: {:?}", self.tick_duration);
+                        self.ready = false;
+                        self.webview.update(Action::ChangeView(0));
+                    }
+                }
+                return Task::none();
+            }
             Message::WebView(action) => self.webview.update(action),
             Message::ViewCreated => {
                 println!("WebView is ready!");
@@ -70,6 +100,9 @@ impl App {
                         screenshot.rgba,
                     ),
                 ));
+                self.state = State::Ticking {
+                    last_tick: std::time::Instant::now(),
+                };
                 return Task::done(Message::Png);
             }
             Message::Png => {
@@ -83,6 +116,18 @@ impl App {
             Message::PngSaved(res) => {
                 self.png_saving = false;
                 self.saved_png_path = Some(res);
+                return Task::done(Message::Scp);
+            }
+            Message::Scp => {
+                if self.scp_sending {
+                    return Task::none();
+                }
+                self.scp_sending = true;
+                tokio::spawn(async {
+                    if let Err(e) = pimoroni_notify().await {
+                        eprintln!("Failed to send notification: {}", e);
+                    }
+                });
                 return Task::none();
             }
         }
@@ -97,11 +142,22 @@ impl App {
     }
 
     fn subscription(&self) -> Subscription<Message> {
+        let tick = match self.state {
+            State::Idle => Subscription::none(),
+            State::Ticking { .. } => time::every(milliseconds(100)).map(|_| Message::Tick),
+        };
+
         Subscription::batch(vec![
+            tick,
             time::every(Duration::from_millis(10))
                 .map(|_| Action::Update)
                 .map(Message::WebView),
-            time::every(Duration::from_secs(1)).map(|_| Message::Screenshot),
+            if self.ready {
+                time::every(Duration::from_secs(1)).map(|_| Message::Screenshot)
+            } else {
+                Subscription::none()
+            },
+            // time::every(Duration::from_secs(1)).map(|_| Message::Screenshot),
         ])
     }
 }
@@ -113,8 +169,10 @@ fn main() -> iced::Result {
         return Ok(());
     }
 
+    // 800x480 window
     iced::application(App::new, App::update, App::view)
         .title("Webview")
+        .window_size(iced::Size::new(800.0, 480.0))
         .subscription(App::subscription)
         .run()
 }
@@ -142,3 +200,19 @@ async fn save_to_png(screenshot: Screenshot) -> Result<String, PngError> {
 
 #[derive(Clone, Debug)]
 struct PngError(String);
+
+async fn pimoroni_notify() -> tokio::io::Result<()> {
+    sh_exec("/home/david/onthoudhet-kiosk.sh").await
+}
+
+async fn sh_exec(command: &str) -> tokio::io::Result<()> {
+    use tokio::process::Command;
+
+    Command::new("bash")
+        .arg("-c")
+        .arg(command)
+        .spawn()?
+        .wait()
+        .await
+        .map(|_| ())
+}
